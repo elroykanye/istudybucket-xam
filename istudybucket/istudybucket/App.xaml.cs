6ed7993f5779e89f122475f8c1b2b983ae90d963
Elroy Kanye <elroykanye@gmail.com>
2021-09-14 17:03:19 +0100

    initial commit - flyout

---
+++ istudybucket/istudybucket/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using istudybucket.Services;
+using istudybucket.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace istudybucket
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
